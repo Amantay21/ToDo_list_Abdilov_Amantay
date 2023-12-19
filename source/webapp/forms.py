44fa5616d6e4a19a463129c8ab0520582a127384
Amantay21 <amanatayabdilov21@gamil.com>
2023-12-19 18:50:04 +0600

    переделал на FormView

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,11 +1,38 @@
 from django import forms
 
-from webapp.models import Status, Type
+from webapp.models import Status, Type, Task
 
 
-class TaskForms(forms.Form):
-    title = forms.CharField(max_length=200, required=True, label='Заголовок')
-    description = forms.CharField(max_length=400, required=True, label='Описание')
-    status = forms.ModelChoiceField(queryset=Status.objects.all(), label='status')
-    types = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), label='Типы')
+class TaskForms(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ('title', 'description', 'status', 'types')
+        widgets = {'types': forms.CheckboxSelectMultiple}
+        error_messages = {
+            'title': {
+                'required': 'Please enter',
+                'min_length': 'Заголовок слишком короткий'
+            }
+        }
+
+        def clean_title(self):
+            title = self.cleaned_data['title']
+            if len(title) < 4:
+                raise forms.ValidationError('Заголовок слишком короткий')
+            return title
+
+        def bad_words(self):
+            bad_words = ['дурак', 'дебил', 'чушпан']
+            title = self.cleaned_data['title']
+            if bad_words in title:
+                raise forms.ValidationError('Нельзя использовать запещенные слова')
+            return title
+# class TaskForms(forms.Form):
+#     title = forms.CharField(max_length=200, required=True, label='Заголовок')
+#     description = forms.CharField(max_length=400, required=True, label='Описание')
+#     status = forms.ModelChoiceField(queryset=Status.objects.all(), label='status')
+#     types = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), label='Типы')
+
+
+
 
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -27,54 +27,50 @@ class TaskView(TemplateView):
 
 class TaskCreateView(TemplateView):
     template_name = 'tasks_create.html'
+    form_class = TaskForms
 
-    def get(self, request, *args, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form'] = TaskForms()
-        return render(request, 'tasks_create.html', context=context)
-
-    def post(self, request, *args, **kwargs):
-        form = TaskForms(data=request.POST)
-        if form.is_valid():
-            types = form.cleaned_data.pop('types')
-            task = Task.objects.create(
-                title=form.cleaned_data['title'],
-                description=form.cleaned_data['description'],
-                status=form.cleaned_data['status'],
-            )
-            task.types.set(types)
-            return redirect('tasks_view', pk=task.pk)
-        return render(request, 'tasks_create.html', {'form': form})
+    def form_valid(self, form):
+        self.task = form.save()
+        return redirect('tasks_view', pk=self.task.pk)
+    # def get(self, request, *args, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['form'] = TaskForms()
+    #     return render(request, 'tasks_create.html', context=context)
+    #
+    # def post(self, request, *args, **kwargs):
+    #     form = TaskForms(data=request.POST)
+    #     if form.is_valid():
+    #         types = form.cleaned_data.pop('types')
+    #         task = Task.objects.create(
+    #             title=form.cleaned_data['title'],
+    #             description=form.cleaned_data['description'],
+    #             status=form.cleaned_data['status'],
+    #         )
+    #         task.types.set(types)
+    #         return redirect('tasks_view', pk=task.pk)
+    #     return render(request, 'tasks_create.html', {'form': form})
 
 
 class TaskUpdateView(TemplateView):
     template_name = 'task_update.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        task = get_object_or_404(Task, pk=kwargs.get('pk'))
-        form = TaskForms(initial={
-            'title': task.title,
-            'description': task.description,
-            'status': task.status,
-            'types': task.types.all()
-        })
-        context['form'] = form
-        return context
+    form_class = TaskForms
 
-    def post(self, request, *args, **kwargs):
-        task = get_object_or_404(Task, pk=kwargs.get('pk'))
-        form = TaskForms(data=request.POST)
-        if form.is_valid():
-            types = form.cleaned_data.pop('types')
-            task.title = form.cleaned_data.get('title')
-            task.description = form.cleaned_data.get('description')
-            task.status = form.cleaned_data.get('status')
-            task.types.set(types)
-            task.save()
-            return redirect('tasks_view', pk=task.pk)
-        else:
-            return render(request, 'task_update.html', {'form': form})
+    def dispatch(self, request, *args, **kwargs):
+        self.task = self.get_object()
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_object(self):
+        return get_object_or_404(Task, pk=self.kwargs.get('pk'))
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['instance'] = self.task
+        return kwargs
+
+    def form_valid(self, form):
+        form.save()
+        return redirect('article_view', pk=self.task.pk)
 
 
 class TaskDeleteView(View):
